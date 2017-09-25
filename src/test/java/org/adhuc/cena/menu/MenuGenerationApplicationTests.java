1d904a37ef42d74379cf0b51c309ad1ad0f263d0
Alexandre Carbenay <acarbenay@adhuc.fr>
2017-09-25 19:02:08 +0200

    Polishing
    Add licence header in all source files

--- src/test/java/org/adhuc/cena/menu/MenuGenerationApplicationTests.java
+++ src/test/java/org/adhuc/cena/menu/MenuGenerationApplicationTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 Alexandre Carbenay
+ *
+ * This file is part of Cena Project.
+ *
+ * Cena Project is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Cena Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Cena Project. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.adhuc.cena.menu;
 
 import org.junit.Test;
@@ -5,12 +20,20 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * The menu generation application launcher test.
+ *
+ * @author Alexandre Carbenay
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MenuGenerationApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
--- src/test/java/org/adhuc/cena/menu/acceptance/MenuGenerationAcceptanceTest.java
+++ src/test/java/org/adhuc/cena/menu/acceptance/MenuGenerationAcceptanceTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 Alexandre Carbenay
+ *
+ * This file is part of Cena Project.
+ *
+ * Cena Project is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Cena Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Cena Project. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.adhuc.cena.menu.acceptance;
 
 import org.junit.runner.RunWith;
@@ -6,7 +21,7 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 /**
- * The menu generation API acceptance tests main executor.
+ * The menu generation acceptance tests main executor.
  *
  * @author Alexandre Carbenay
  *
