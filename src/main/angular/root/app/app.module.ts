e5e6346088783818e6c1d63289f0b629c32d5cdd
Alexandre Carbenay <acarbenay@adhuc.fr>
2017-12-12 22:25:57 +0100

    Define core module with header and footer components
    Header and footer empty for now

--- src/main/angular/root/app/app.module.ts
+++ src/main/angular/root/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from "./app.component";
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
+  ],
+  declarations: [
+    AppComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/main/angular/root/app/core/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "cena-footer",
+  templateUrl: "./footer.component.html"
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
