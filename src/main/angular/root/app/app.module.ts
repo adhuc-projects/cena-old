c57f7636c701287efedb489313ae75a094616fa4
Alexandre Carbenay <acarbenay@adhuc.fr>
2017-12-16 14:00:11 +0100

    Include angular material and bootstrap dependencies

--- src/main/angular/root/app/app.module.ts
+++ src/main/angular/root/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "@app/app-routing.module";
@@ -10,6 +11,7 @@ import { AppComponent } from "@app/app.component";
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
     SearchModule
